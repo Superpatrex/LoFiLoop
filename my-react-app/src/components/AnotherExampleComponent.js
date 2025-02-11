765cb014a88ce5619093be0cfd384619a8ef960c
superpatrex <jack@ding911.com>
2025-02-11 17:26:04 -0500

    Initial

---
+++ my-react-app/src/components/AnotherExampleComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AnotherExampleComponent = () => {
+    return (
+        <div>
+            <h3>Man this is already perfect why would I change it?</h3>
+        </div>
+    );
+};
+
+export default AnotherExampleComponent;
---
+++ my-react-app/src/components/ExampleComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ExampleComponent = () => {
+    return (
+        <div>
+            <h1>OMG I ALSO THINK JACK IS SO COOL</h1>
+        </div>
+    );
+};
+
+export default ExampleComponent;

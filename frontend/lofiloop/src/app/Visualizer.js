1dab764c8be4d2a9627ed54a3ae9468f471c7bbd
0zzy4 <jessica.ozzy@yahoo.com>
2025-02-16 01:56:15 -0500

    setting up p5

---
+++ frontend/lofiloop/src/app/Visualizer.js
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from "react";
+import p5 from "p5";
+
+const P5Wrapper = ({ sketch }) => {
+    const p5Container = useRef(null);
+
+    useEffect(() => {
+        // Create new P5 instance
+        const p5Instance = new p5(sketch, p5Container.current);
+
+        // Cleanup on unmount
+        return () => {
+            p5Instance.remove();
+        };
+    }, [sketch]);
+
+    return <div ref={p5Container} />;
+};
+
+export default P5Wrapper;

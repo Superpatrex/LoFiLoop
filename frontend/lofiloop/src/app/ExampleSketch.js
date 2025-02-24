fe429d310bd5f7b5a822c9157ac49aaf53aeff4b
0zzy4 <jessica.ozzy@yahoo.com>
2025-02-23 22:19:41 -0500

    p5 test working, but if Visualizer.js line 3 is uncommented, there's a runtime error. Not sure how to import p5's sound library

--- frontend/lofiloop/src/app/ExampleSketch.js
+++ frontend/lofiloop/src/app/ExampleSketch.js
@@ -13,31 +13,31 @@ const ExampleSketch = () => {
     const sketch = (p) => {
         let song;
 
-        p.preload = () => {
-            song = p.loadSound("Lukrembo_Biscuit.mp3");
-        };
+        // p.preload = () => {
+        //     song = p.loadSound("Lukrembo_Biscuit.mp3");
+        // };
 
         p.setup = () => {
             p.createCanvas(p.windowWidth, p.windowHeight);
             const lavender = p.color(172, 143, 227);
             p.background(lavender);
-            song.play();
+            // song.play();
         };
 
-        p.mouseClicked = () => {
-            if (song.isPlaying()) {
-                song.pause();
-            } else {
-                song.play();
-            }
-        }
+        // p.mouseClicked = () => {
+        //     if (song.isPlaying()) {
+        //         song.pause();
+        //     } else {
+        //         song.play();
+        //     }
+        // }
 
 
-        // // P5.js draw loop
-        // p.draw = () => {
-        //     p.fill(255, 0, 0);
-        //     p.ellipse(p.mouseX, p.mouseY, 20, 20);
-        // };
+        // P5.js draw loop
+        p.draw = () => {
+            p.ellipse(p.mouseX, p.mouseY, 20, 20);
+            p.fill(255, 0, 0);
+        };
     };
 
     return (
--- frontend/lofiloop/src/app/Visualizer.js
+++ frontend/lofiloop/src/app/Visualizer.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
 import p5 from "p5";
-import "p5/lib/addons/p5.sound"; 
+// import "p5/lib/addons/p5.sound";
 
 const P5Wrapper = ({ sketch }) => {
     const p5Container = useRef(null);

08a36d3129462b501eb5fdfb91eeb32143667c60
eson39 <eson39@gatech.edu>
2025-02-24 19:36:14 -0500

    Chat
    added chat

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,31 +1,11 @@
-import React from 'react';
-import ExampleComponent from './components/ExampleComponent';
-import AnotherExampleComponent from './components/AnotherExampleComponent';
-import jackImage from './literally_jack.webp';
-import alsoJackImage from './jack_rizz.webp';
-import sound from './rizz-sounds.mp3';
-import AMONG_US from './AMONG_US.mp3';
+import Chat from "./components/Chat";
 
 function App() {
-    return (
-        <div>
-            <h1>Welcome to a React App MADE BY JACK NO WAY....WOW! JACK IS SO COOL</h1>
-            <ExampleComponent />
-            <AnotherExampleComponent />
-            <audio controls loop autoPlay>
-                <source src={sound} type="audio/mpeg" />
-                Your browser does not support the audio element.
-            </audio>
-            <audio controls loop autoPlay>
-                <source src={AMONG_US} type="audio/mpeg" />
-                Your browser does not support the audio element.
-            </audio>
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <img src={jackImage} alt="Jack" />
-                <img src={alsoJackImage} alt="Jack" />
-            </div>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Chat />
+    </div>
+  );
 }
 
 export default App;

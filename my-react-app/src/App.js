f0d8cd1f4b0b7287892e6a4e95930e1da0ee22bb
Krupa Patel <krupatel64@gmail.com>
2025-02-25 19:28:14 -0500

    Created Page title and sign up page
    Blah blah blah page title stuff

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -5,26 +5,15 @@ import jackImage from './literally_jack.webp';
 import alsoJackImage from './jack_rizz.webp';
 import sound from './rizz-sounds.mp3';
 import AMONG_US from './AMONG_US.mp3';
+import SignUp from './components/SignUp';
+import PageTitle from './components/PageTitle';
 
 function App() {
     return (
-        <div>
-            <h1>Welcome to a React App MADE BY JACK NO WAY....WOW! JACK IS SO COOL</h1>
-            <ExampleComponent />
-            <AnotherExampleComponent />
-            <audio controls loop autoPlay>
-                <source src={sound} type="audio/mpeg" />
-                Your browser does not support the audio element.
-            </audio>
-            <audio controls loop autoPlay>
-                <source src={AMONG_US} type="audio/mpeg" />
-                Your browser does not support the audio element.
-            </audio>
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <img src={jackImage} alt="Jack" />
-                <img src={alsoJackImage} alt="Jack" />
-            </div>
-        </div>
+        <>
+            <PageTitle />
+            <SignUp />
+        </>
     );
 }
 

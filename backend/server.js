6a55fc1a045af8570560b45376daeb2280156401
Aumrita Maitra <aums3937@gmail.com>
2025-02-25 19:19:24 -0500

    included user registration and password authetication

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3001;
+const express = require("express");
+const connectDB = require("./db");
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+require("dotenv").config();
 
+const app = express();
+app.use(cors());
 app.use(express.json());
 
-app.get('/api', (req, res) => {
-  res.json({ message: 'OMG JACK IS SOOOOO COOL AND FUNNY!!!!' });
-});
+connectDB();
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.use("/auth", authRoutes);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
 });

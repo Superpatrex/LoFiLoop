5c122b727bd5f7826ea33da610eb4a995d615905
eson39 <eson39@gatech.edu>
2025-02-26 19:36:24 -0500

    Song UI added
    added song ui

--- frontend/lofiloop/src/app/page.js
+++ frontend/lofiloop/src/app/page.js
@@ -3,12 +3,12 @@ import ExampleSketch from "./ExampleSketch";
 import ForgotPassword from "./ForgotPassword";
 import SignUp from "./SignUp";
 import PageTitle from "./PageTitle";
+import Chat from "./Chat"
 
 export default function Home() {
   return (
     <>
-      <PageTitle/>
-      <SignUp/>
+      <Chat/>
     </>
     );
 }

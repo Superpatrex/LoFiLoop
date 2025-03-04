8ce75f99ac888bba3358571dd1c6d84d8c19fc7f
eson39 <eson39@gatech.edu>
2025-03-04 16:04:50 -0500

    Play songs
    user can now play songs

--- frontend/lofiloop/src/app/page.js
+++ frontend/lofiloop/src/app/page.js
@@ -3,12 +3,12 @@ import ExampleSketch from "../components/ExampleSketch";
 import ForgotPassword from "../components/ForgotPassword";
 import SignUp from "../components/SignUp";
 import PageTitle from "../components/PageTitle";
+import Chat from "../components/Chat";
 
 export default function Home() {
   return (
     <>
-      <PageTitle/>
-      <SignUp/>
+     <Chat/>
     </>
     );
 }

86e1c14e56b8335336322a95e384f09b17e982d9
eson39 <eson39@gatech.edu>
2025-03-04 16:19:10 -0500

    Deleted duplicate Chat
    Had 2 chats at once

--- frontend/lofiloop/src/app/page.js
+++ frontend/lofiloop/src/app/page.js
@@ -1,14 +1,15 @@
 import Image from "next/image";
-import ExampleSketch from "./ExampleSketch";
-import ForgotPassword from "./ForgotPassword";
-import SignUp from "./SignUp";
-import PageTitle from "./PageTitle";
-import Chat from "./Chat"
+import ExampleSketch from "../components/ExampleSketch";
+import ForgotPassword from "../components/ForgotPassword";
+import SignUp from "../components/SignUp";
+import PageTitle from "../components/PageTitle";
+import Chat from "../components/Chat";
 
 export default function Home() {
   return (
     <>
-      <Chat/>
+      <PageTitle/>
+      <SignUp/>
     </>
     );
 }

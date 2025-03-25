47a9ab990498c85049d368eac09128f9631ddf5f
Aumrita Maitra <aums3937@gmail.com>
2025-03-25 13:56:13 -0400

    finished aboutus file and moved to routing folder in app

--- frontend/lofiloop/src/app/page.js
+++ frontend/lofiloop/src/app/page.js
@@ -3,6 +3,7 @@ import ExampleSketch from "../components/ExampleSketch";
 import ForgotPassword from "../components/ForgotPassword";
 import SignUp from "../components/SignUp";
 import PageTitle from "../components/PageTitle";
+import AboutUs from "./aboutus/AboutUs";
 
 export default function Home() {
   return (

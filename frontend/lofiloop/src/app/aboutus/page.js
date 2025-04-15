1e289f0268702ce2d4be4caf4aa3d3950e2edfb6
Krupa Patel <krupatel64@gmail.com>
2025-04-15 19:00:27 -0400

    Added AboutUs Routing
    Added AboutUs.js and AboutUs.css files and routed.

---
+++ frontend/lofiloop/src/app/aboutus/page.js
@@ -0,0 +1,10 @@
+import AboutUs from "../components/AboutUs";
+import Layout from "../components/Layout";
+
+export default function LogInPage() {
+  return (
+    <Layout>
+      <AboutUs />
+    </Layout>
+  );
+}
--- frontend/lofiloop/src/app/login/page.js
+++ frontend/lofiloop/src/app/login/page.js
@@ -7,10 +7,6 @@ export default function LogInPage() {
   return (
     <Layout>
       <LogIn />
-      <div className="navigation-links">
-        <p>Don't have an account? <Link href="/signup">Sign Up Here</Link></p>
-        <p>Forgot your password? <Link href="/forgot-password">Reset Password</Link></p>
-      </div>
     </Layout>
   );
 }
